4f9a7cc745f6cdf2b34e3ec027d675ba4b6b3cfd
masiljangajji <xmfpdlsj0508@gmail.com>
2025-09-02 21:20:18 +0900

    feat: MarkdownController 추가

---
+++ src/main/java/com/dawn/cs_study/content/api/MarkdownController.java
@@ -0,0 +1,39 @@
+package com.dawn.cs_study.content.api;
+
+import com.dawn.cs_study.content.application.ContentReaderService;
+import com.dawn.cs_study.content.domain.Slug;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MarkdownController {
+
+    private final ContentReaderService contentReaderService;
+
+    @GetMapping("/favicon.ico")
+    public void favicon() {
+        // 아무 것도 반환 안 함
+    }
+
+    @GetMapping("/{title}")
+    public String test(@PathVariable(name = "title") String title) throws Exception {
+
+        // 1-1. CDN 체크 후 없으면 html 렌더
+        // 1-2. CDN 체크 있으면 바로 제공
+        // 2. 결과물 CDN 으로 업로드 후 제공
+
+        // 현재는 S3에서 가져오는 중
+        return contentReaderService.renderHtml("cs/content/os/test.md");
+    }
+
+
+    @GetMapping("dd")
+    public Slug slug() {
+        return contentReaderService.readSlug("cs/content/os/test.json");
+    }
+
+}
+
