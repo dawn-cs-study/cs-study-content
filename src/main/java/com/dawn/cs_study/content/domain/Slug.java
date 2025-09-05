b5f32c046eef81bad3d5445aca15f3b3492ea82e
masiljangajji <xmfpdlsj0508@gmail.com>
2025-09-05 23:34:10 +0900

    refactor: Slug 변경 (record -> class)

--- src/main/java/com/dawn/cs_study/content/domain/Slug.java
+++ src/main/java/com/dawn/cs_study/content/domain/Slug.java
@@ -1,11 +1,26 @@
 package com.dawn.cs_study.content.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
 import java.util.List;
 
-public record Slug(
-        String title,
-        String category,
-        List<String> tags,
-        String author
-) {
+@Entity
+@Getter
+public class Slug {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @ElementCollection
+    private List<String> tags;
+
+    private String summary;
+    private String author;
+
 }
