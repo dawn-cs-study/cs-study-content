2d143312efc285d3cc1ac321bf073eb412347503
masiljangajji <xmfpdlsj0508@gmail.com>
2025-09-12 02:04:34 +0900

    init: lambda 에서 css,js,deploy 분리

---
+++ src/test/java/com/dawn/cs_study_content/CsStudyContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.cs_study_content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsStudyContentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
